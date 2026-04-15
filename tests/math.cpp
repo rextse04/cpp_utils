2d7e2eb50ebd02acb6dc9bfe25bc4774f81dc809
Rex Tse <rextse.work@gmail.com>
2026-04-15 18:10:40 +0800

    improve math.hpp interface

--- tests/math.cpp
+++ tests/math.cpp
@@ -8,6 +8,6 @@ BOOST_AUTO_TEST_CASE(pow_test) {
     BOOST_CHECK_EQUAL(utils::pow<3>(2), 8);
     BOOST_CHECK_EQUAL(utils::pow<0>(2), 1);
     BOOST_CHECK_EQUAL(utils::pow<-1>(2), 0);
-    BOOST_CHECK_EQUAL(utils::pow<-1>(2, as<float>{}), 0.5);
-    BOOST_CHECK_NE(utils::pow<-1>(3, as<float>{}), utils::pow<-1>(3, as<double>{}));
+    BOOST_CHECK_EQUAL((utils::pow<-1, float>(2)), 0.5);
+    BOOST_CHECK_NE((utils::pow<-1, float>(3)), (utils::pow<-1, double>(3)));
 }
